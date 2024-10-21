4b95d9705196e2d2afb4bf1cc15eae08a45c6deb
Saad Sahir <133065206+saad-sahir@users.noreply.github.com>
2024-10-22 00:05:42 +0530

    Update revsc2.py

--- revsc2.py
+++ revsc2.py
@@ -6,6 +6,7 @@ from ultralytics import YOLO
 
 def person_loitering(video):
     model = YOLO("weights/yolov9t.pt")
+    package_model = YOLO("weights/package.pt")
     cap = cv2.VideoCapture(video)
     start_times = defaultdict(lambda: None)
     triggers = defaultdict(lambda: False)
@@ -27,7 +28,47 @@ def person_loitering(video):
         current_time = current_frame / fps
         if current_frame % 1 == 0:
             results = model.track(frame, persist=True, verbose=False)
+            results2 = package_model.track(frame, persist=True, verbose=True)
             detections = results[0].boxes.data
+            packages = results2[0].boxes.data
+            for package in packages:
+                if len(detection) == 7:
+                    x_min, y_min, x_max, y_max, id, confidence, class_id = detection
+                else:
+                    x_min, y_min, x_max, y_max, confidence, class_id = detection
+                    id = None
+                    person_id = int(id)
+                    cv2.putText(
+                        frame,
+                        f"({int(id)})",
+                        (int(x_min), int(y_min)-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2
+                    )
+                    cv2.rectangle(
+                        frame,
+                        (int(x_min), int(y_min)),
+                        (int(x_max), int(y_max)),
+                        (255, 0, 0), 4
+                    )
+                    if not triggers[person_id]:
+                        triggers["-1"] = True
+                        start_times["-1"] = current_time
+                    elapsed_time = current_time - start_times[person_id]
+                    text = f"Duration : {int(elapsed_time)}s"
+                    cv2.putText(
+                        frame,
+                        text,
+                        (int(x_max) - 100, int(y_min) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255)
+                    )
+                    if int(elapsed_time) >= 10 and not notifications[person_id]: 
+                        # api_post = api_post_template
+                        # api_post["event_date"] = datetime.datetime.now().isoformat()
+                        # resized_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+                        # _, buffer = cv2.imencode('.jpg', resized_frame)
+                        # api_post["image"] = base64.b64encode(buffer).decode('utf-8')
+                        print("Notification triggered")
+                        notifications["-1"] = True
             for detection in detections:
                 if len(detection) == 7:
                     x_min, y_min, x_max, y_max, id, confidence, class_id = detection
@@ -67,6 +108,7 @@ def person_loitering(video):
                         # api_post["image"] = base64.b64encode(buffer).decode('utf-8')
                         print("Notification triggered")
                         notifications[person_id] = True
+                
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'): break
     cap.release()
